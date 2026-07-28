79e77ad15baa1670afe866317dd177eb6289e631
BHEEMANAGOUDA PATIL <corporatejobsnetwork@gmail.com>
2026-07-28 23:02:07 +0530

    Fix Firebase configuration

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -13,8 +13,12 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = getApps().length
+  ? getApp()
+  : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+export default app;
