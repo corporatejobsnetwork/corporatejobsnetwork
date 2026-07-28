58baa90722f37f6a9fede70e3aa152bfe718b373
BHEEMANAGOUDA PATIL <corporatejobsnetwork@gmail.com>
2026-07-28 23:25:05 +0530

    Fix Firebase configuration

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+  apiKey: "AIzaSyDjXQSgyn5gTnSgePaXowhhkIGv8cWFG4s",
+  authDomain: "corporate-jobs-network-prod.firebaseapp.com",
+  projectId: "corporate-jobs-network-prod",
+  storageBucket: "corporate-jobs-network-prod.firebasestorage.app",
+  messagingSenderId: "211326481215",
+  appId: "1:211326481215:web:65ae79ef9efff52cc8d98a",
+  measurementId: "G-8JFTK5K3C6",
+}
 
 const app = getApps().length
   ? getApp()
